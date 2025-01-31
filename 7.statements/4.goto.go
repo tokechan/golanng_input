e21ddc0988ef2d099d16d97f66bb4ded426af746
toketime <flatoke@gmail.com>
2025-01-31 22:54:59 +0900

    defer

---
+++ 7.statements/4.goto.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+//ラベル付きfor
+
+func main() {
+
+Loop:
+	for i := 0; i < 3; i++ {
+		for j := 1; j < 3; j++ {
+			if j > 1 {
+				continue Loop
+			}
+			fmt.Println(i, j, i*j)
+		}
+		fmt.Println("処理をしない")
+	}
+}
