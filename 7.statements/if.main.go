46dbc9018aed80225df01c1c87cde0660ee1c3a3
toketime <flatoke@gmail.com>
2025-01-31 21:31:14 +0900

    err handling

---
+++ 7.statements/if.main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+// if文
+func main() {
+	a := 2
+	if a == 2 {
+		fmt.Println("two")
+	} else if a == 1 {
+		fmt.Println("one")
+	} else {
+		fmt.Println("I don't know")
+	}
+
+}
